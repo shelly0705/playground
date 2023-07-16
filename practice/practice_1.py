cce16eca5fa8e5b030bcf49fb8bfe21f8a1e0935
shelly0705 <139233670+shelly0705@users.noreply.github.com>
2023-07-17 01:59:44 +0800

    add 2 practice files

--- practice/practice_1.py
+++ practice/practice_1.py
@@ -1,6 +1,8 @@
+# 變數名稱只能有英文字母、數字、底線。 變數名稱可以字母底和底線開頭,但不能以數字起頭
+# 變數名稱裡不能有空格
+
 message = "hello python world!"
 print(message)
 
 message = "hello python crash course world!"
 print(message)
-
---
+++ practice/practice_2.py
@@ -0,0 +1,13 @@
+name = "name"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+frist_name = "shelly"
+last_name = "kao"
+full_name = frist_name + " " + last_name
+massage = "hello," + full_name.title() + "!"
+print(massage)
+
+
+print(name.title() + ":\n\tkao shelly.".title())
