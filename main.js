a0334752c1fdc1be146e94b929a69db91dc41073
Jimmy Lin <b00502013@gmail.com>
2023-12-19 00:10:04 +0800

    Add a JavaScript program

---
+++ main.js
@@ -0,0 +1,11 @@
+function printEven(numbers) {
+  for (number of numbers) {
+    if (number % 2 === 0) {
+      console.log(number);
+    } else {
+    }
+  }
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+printEven(numbers);
