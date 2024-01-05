c29b11b3bdc8f13e030dd026eb60993c68abd0bd
shelly0705 <139233670+shelly0705@users.noreply.github.com>
2024-01-05 21:18:05 +0800

    add homework

---
+++ homework.js
@@ -0,0 +1,9 @@
+const correctAnswers = ["A", "B", "D", "C", "A"];
+
+const jimmyAnswer = ["A", "B", "A", "B", "A"];
+
+function getScore(studentAnswer) {
+  // Your code
+}
+
+console.log(getScore(jimmyAnswer)); // should print 60
