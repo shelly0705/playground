16a4d4d45ab733c445b780e61e075e90cedfcaa6
shelly0705 <139233670+shelly0705@users.noreply.github.com>
2024-01-05 21:13:45 +0800

    add some basic examples

---
+++ basic-javscript.js
@@ -0,0 +1,8 @@
+// In Python, a = 1
+let a = 1;
+console.log(a);
+a = 2;
+a = 3;
+// Can't be changed
+const b = 1;
+console.log(a);
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+// Select the button we have in the `index.html`
+const button = document.querySelector("#myFirstDiv");
+
+// console.log("I have selected the button. I will print it below...");
+// console.log(button);
+let count = 0;
+
+// Add a event listener on it
+const eventType = "click";
+function eventHandler() {
+  button.style = "visibility: hidden";
+}
+button.addEventListener(eventType, eventHandler);
