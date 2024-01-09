def835e035a4146486051c6c3e9aae9c73f695de
shelly0705 <139233670+shelly0705@users.noreply.github.com>
2024-01-09 10:15:33 +0800

    text

--- homework.js
+++ homework.js
@@ -1,9 +1,12 @@
 const correctAnswers = ["A", "B", "D", "C", "A"];
 
 const jimmyAnswer = ["A", "B", "A", "B", "A"];
+const shellyAnswer = ["C", "C", "C", "C", "C"];
 
 function getScore(studentAnswer) {
   // Your code
+  return 100;
 }
 
 console.log(getScore(jimmyAnswer)); // should print 60
+console.log(getScore(shellyAnswer)); // should print 20
