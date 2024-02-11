0e79652b3038dd8f1db455e9da9541136649df54
shelly0705 <139233670+shelly0705@users.noreply.github.com>
2024-02-12 04:03:12 +0800

    new

--- README.md
+++
@@ -1,4 +0,0 @@
-# Homework 01/22
-
-1. Get familar with for look and function
-2. Get familar with high-order function
--- functionText.js
+++
@@ -1,8 +0,0 @@
-// 函式 function，為預先寫好的程式碼
-function hello(){
-    document.write("你好");
-}
-
-//define一個名為hello的函式，而程式碼為document.write("你好");
-//函式需被呼叫，才會被執行
-hello();
